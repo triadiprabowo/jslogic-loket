11f6354565826e0ebb506bed35a774a15bd007ba
Triadi Prabowo <triadiprabowo@gmail.com>
2018-08-13 06:00:28 +0700

    [Temp]: Save project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		RouterModule.forRoot([
 			{ path: '', redirectTo: 'order-application-form', pathMatch: 'full' },
 			{ path: 'order-application-form', loadChildren: './pages/order-application/order-application.module#OrderApplicationModule' },
